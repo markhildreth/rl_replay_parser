d2ef9ae1910c3553327d6620b388a3b814991ec9
Mark Hildreth <mark.k.hildreth@gmail.com>
2015-07-30 19:09:35 -0400

    Parse level info correctly

--- rl_replay_parser.py
+++ rl_replay_parser.py
@@ -8,17 +8,25 @@ class ReplayParser:
 
     def parse(self, replay_file):
         data = {}
+        # TODO: CRC, version info, other stuff
         unknown = replay_file.read(20)
         header_start = replay_file.read(24)
 
         data['header'] = self._read_properties(replay_file)
-
-        unknown = self._read_unknown(replay_file, 12)
-        map_name_length = self._read_integer(replay_file, 4)
-        map_name = self._read_string(replay_file, map_name_length)
-
+        unknown = self._read_unknown(replay_file, 8)
+        data['level_info'] = self._read_level_info(replay_file)
         data['key_frames'] = self._read_key_frames(replay_file)
         return data
+
+    def _sniff_bytes(self, replay_file, size):
+        b = self._read_unknown(replay_file, size)
+        print("**** BYTES ****")
+        print("Bytes: {}".format(self._pretty_byte_string(b)))
+        if size == 2:
+            print("Short: Signed: {} Unsigned: {}".format(struct.unpack('<h', b), struct.unpack('<H', b)))
+        else:
+            print("Integer: Signed: {}, Unsigned: {}".format(struct.unpack('<i', b), struct.unpack('<I', b)))
+            print("Float: {}".format(struct.unpack('<f', b)))
 
     def _read_properties(self, replay_file):
         results = {}
@@ -74,14 +82,26 @@ class ReplayParser:
 
         return { 'name' : property_name, 'value': value}
 
+    def _read_level_info(self, replay_file):
+        map_names = []
+        number_of_maps = self._read_integer(replay_file, 4)
+        for x in range(number_of_maps):
+            map_name_length = self._read_integer(replay_file, 4)
+            map_name = self._read_string(replay_file, map_name_length)
+            map_names.append(map_name)
+
+        return map_names
+
     # I'm not sure if they're actually called "key frames", but it seems to be a 
     # list of frames, along with some other number...
     def _read_key_frames(self, replay_file):
         number_of_key_frames = self._read_integer(replay_file, 4)
-        return [
+        key_frames = [
             self._read_key_frame(replay_file)
             for x in range(number_of_key_frames)
         ]
+        zero_byte = self._read_unknown(replay_file, 1)
+        return key_frames
 
     def _read_key_frame(self, replay_file):
         time = self._read_float(replay_file, 4)
